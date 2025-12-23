f7fcd8908bccce391deb6ee1540c32bdedd84436
Ritik Raj <ritikraj1369@gmail.com>
2025-12-23 23:47:50 +0530

    done

--- components/info.tsx
+++ components/info.tsx
@@ -15,17 +15,6 @@ const Info = ({ data }: { data: Product }) => {
       </div>
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
-        <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold text-black">Size:</h3>
-          <div>{data?.size?.name}</div>
-        </div>
-        <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold text-black">Color:</h3>
-          <div
-            className="h-6 w-6 rounded-full border border-gray-600"
-            style={{ backgroundColor: data?.color?.value }}
-          ></div>
-        </div>
         {data.filterItems.map((filter) => (
           <div className="flex items-center gap-x-4">
             <h3 className="font-semibold text-black">
